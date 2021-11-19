44a0681c32045a0df15e483cf45c8b7eadb6c18e
Naha192839 <44591899+Naha192839@users.noreply.github.com>
2021-11-19 23:06:49 +0900

    add two_image multipul generator

--- 3C-CNN.py
+++ 3C-CNN.py
@@ -20,7 +20,7 @@ nb_train_samples = 1400
 nb_validation_samples = 200
 img_width, img_height = 224, 224
 
-train_batch_size = 16
+train_batch_size = 64
 val_batch_size = 16
 
 # train用
@@ -64,13 +64,50 @@ test_generator = test_datagen.flow_from_directory(
   batch_size=val_batch_size # 1回のバッチ生成で作る画像数
   )
 
-def generator_two_img():
-    genX1 = train_generator
-    genX2 = train_generator
+
+def generate_generator_multiple(generator,dir1, dir2, batch_size, img_height,img_width):
+    genX1 = generator.flow_from_directory(dir1,
+                                          target_size = (img_height,img_width),
+                                          color_mode='rgb',
+                                          class_mode = 'categorical',
+                                          classes=classes,
+                                          batch_size = batch_size,
+                                          shuffle=True, 
+                                          seed=7)
+    genX2 = generator.flow_from_directory(dir2,
+                                          target_size = (img_height,img_width),
+                                          color_mode='rgb',
+                                          class_mode = 'categorical',
+                                          classes=classes,
+                                          batch_size = batch_size,
+                                          shuffle=True, 
+                                          seed=7)
     while True:
-        X1i = genX1.next()
-        X2i = genX2.next()
-        yield [X1i[0], X2i[0]], X1i[1]
+            X1i = genX1.next()
+            X2i = genX2.next()
+            yield [X1i[0], X2i[0]], X2i[1]  #Yield both images and their mutual label
+            
+            
+multi_train_generator=generate_generator_multiple(train_datagen,
+                                           dir1=train_data_dir,
+                                           dir2=train_data_dir,
+                                           batch_size=train_batch_size,
+                                           img_height=img_height,
+                                           img_width=img_height)       
+     
+multi_validation_generator=generate_generator_multiple(val_datagen,
+                                          dir1=validation_data_dir,
+                                          dir2=validation_data_dir,
+                                          batch_size=val_batch_size,
+                                          img_height=img_height,
+                                          img_width=img_height)          
+
+multi_test_generator=generate_generator_multiple(test_datagen,
+                                          dir1=test_data_dir,
+                                          dir2=test_data_dir,
+                                          batch_size=val_batch_size,
+                                          img_height=img_height,
+                                          img_width=img_height)
 
 global_input_tensor = Input(shape=(img_height,img_width,3))
 top_input_tensor = Input(shape=(img_height,img_width,3))
@@ -80,6 +117,10 @@ bottom_input_tensor = Input(shape=(img_height,img_width,3))
 vgg16 = VGG16(include_top=False, weights='imagenet',input_tensor=global_input_tensor)
 vgg16.trainable = False
 
+for layer in vgg16.layers[:15]:
+   layer.trainable = False
+for layer in vgg16.layers[15:]:
+   layer.trainable = True
 # resnet50 = ResNet50(include_top=False, weights='imagenet',input_tensor=global_input_tensor)
 # resnet50.trainable = False
 
@@ -133,13 +174,16 @@ bottom_model = Conv2D(1024,(3, 3),strides=(2, 2), activation='relu')(bottom_mode
 bottom_model = Model(bottom_input_tensor,bottom_model)
 
 # -----------------------------------------------------
+input_model1 = top_model
+input_model2 = bottom_model
 
-model = concatenate([bottom_model.output,top_model.output],axis=3)
+model = concatenate([input_model1.output,input_model2.output],axis=3)
 model = AveragePooling2D((7,7))(model)
 model = Flatten()(model)
+model = Dense(256)(model)
 prediction = Dense(nb_classes, activation='softmax')(model)
 
-model = Model([bottom_model.input,top_model.input],prediction)
+model = Model([input_model1.input,input_model2.input],prediction)
 model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.SGD(lr=0.001, momentum=0.9,decay=0.0002),
               metrics=['accuracy'])
@@ -148,10 +192,38 @@ model.summary()
 plot_model(model, show_shapes=True,show_layer_names=False,to_file='model.png')
 
 history = model.fit_generator(
-  generator = generator_two_img(), 
-  steps_per_epoch = nb_train_samples // train_batch_size,
-#   validation_data = [validation_generator,validation_generator],
-#   validation_steps = nb_validation_samples // val_batch_size,
-  epochs=1,
+  multi_train_generator, 
+  steps_per_epoch = train_generator.n // train_batch_size,
+  validation_data = multi_validation_generator,
+  validation_steps = validation_generator.n // val_batch_size,
+  epochs=5,
 #   callbacks=[early_stopping,reduce_lr]
 )
+
+# Evaluate the model on the test data using `evaluate`
+print("Evaluate on test data")
+results = model.evaluate_generator(
+  multi_test_generator,
+  steps=test_generator.n // val_batch_size)
+print("test loss, test acc:", results)
+
+# Plot training & validation accuracy values
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Val'], loc='upper left')
+plt.savefig(os.path.join("./fig/acc_fig/",str(datetime.datetime.today())+"acc.jpg"))
+plt.clf()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Val'], loc='upper left')
+plt.savefig(os.path.join("./fig/loss_fig/",str(datetime.datetime.today())+"loss.jpg"))
+
+model.save('./model/3C-CNN.h5')    
